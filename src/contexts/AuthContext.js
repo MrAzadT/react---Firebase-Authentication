70331f46e34593934448088d2379b938a4752fdb
azadtamal <mrazadt@gmail.com>
2021-10-11 15:46:54 +0600

    AppRouter

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -4,6 +4,8 @@ import { auth } from "./../utils/init.firebase";
 const AuthContext = createContext({
   currentUser: null,
 });
+export const useAuth = () => useContext(AuthContext);
+
 export default function AuthContextProvider({ children }) {
   const { currentUser, SetCurrentUser } = useState(null);
   const value = {
