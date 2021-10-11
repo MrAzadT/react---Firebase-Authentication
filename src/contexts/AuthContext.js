798a96b9c6881fc443b007b1262da2b111d394b3
azadtamal <mrazadt@gmail.com>
2021-10-11 15:41:53 +0600

    function

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,13 @@
+import { useState, useEffect, useContext, createContext } from "react";
+import { auth } from "./../utils/init.firebase";
+
+const AuthContext = createContext({
+  currentUser: null,
+});
+export default function AuthContextProvider({ children }) {
+  const { currentUser, SetCurrentUser } = useState(null);
+  const value = {
+    currentUser,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
